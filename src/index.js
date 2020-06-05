c90038858f334aa4fff38a195c125a7ed3e9fc49
MASB <affansaied@gmail.com>
2020-06-05 20:31:01 +0500

    Assignment1ReactBook
    Write a new component called MediaCard that accepts 3 props: title, body,
    and imageUrl. Inside the component, render the title in an <h2> tag, the body
    in a <p> tag, and pass the imageUrl into an img tag like <img src=
    {imageUrl}/> . Can you return all 3 of these things at once? Or do you need to
    wrap them in another element?
    Render the MediaCard with the ReactDOM.render call, and pass in the
    necessary props. Can you pass a JSX element as a prop value? (hint: wrap it in
    single braces). Try bolding some parts of the body text without changing the
    implementation of MediaCard.
    Make a component called Gate that accepts 1 prop called “isOpen”. When
    isOpen is true, make the component render
    “
    open”
    , and when isOpen is false,
    make it render
    “
    closed”. Hint: you can do conditional logic inside JSX with the
    ternary (question mark, ?) operator, inside single braces, like this: {speed >
    80 ? "danger!" : "probably fine"} (which evaluates to
    “danger!” if speed
    is over 80, and “
    probably fine” otherwise).

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import './index.css';
+//import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+
+function Mediacard(props){
+
+  return(<div>MediaTitle is {props.title} contains body data= {props.body} where image={props.imageurl} </div>);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+<Mediacard title="React book title" body="React body" imageurl="https://360x7.com/media/image/329/maxstone-smart-wireless-camera-remote.jpg"/>
+, document.querySelector('#root')
+//,  document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
