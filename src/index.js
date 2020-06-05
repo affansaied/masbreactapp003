cbc69937a05e25a7cb1db4c83c5cfba7960ca0ac
MASB <affansaied@gmail.com>
2020-06-05 20:44:06 +0500

    reactPractice1
    reactPractice1

--- src/index.js
+++ src/index.js
@@ -10,10 +10,17 @@ function Mediacard(props){
   return(<div>MediaTitle is <h1>{props.title}</h1> <br/>contains body <h2>data= {props.body} </h2> <br/> where image=<h3>{props.imageurl}</h3> </div>);
 }
 
+function Isopen(props){
+
+return(<div> check session open = {props.opened==true? "Open" : "Closed"} </div>);
+}
+
 ReactDOM.render(
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
+<Isopen opened='True' />
+,
 <Mediacard title="React book title" body="React body" imageurl="https://360x7.com/media/image/329/maxstone-smart-wireless-camera-remote.jpg"/>
 , document.querySelector('#root')
 //,  document.getElementById('root')
